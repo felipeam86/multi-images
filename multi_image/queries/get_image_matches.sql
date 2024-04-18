b054759f7e7e6e383b0ba952c02b58d864d74bb4
Felipe Aguirre Martinez <felipe@prediktia.ai>
2024-04-18 15:24:48 -0500

    🗃️✨ Add first and last sale for search and match products

--- multi_image/queries/get_image_matches.sql
+++ multi_image/queries/get_image_matches.sql
@@ -9,6 +9,20 @@ WITH thumbnail_imgs AS (
     WHERE
         pi.type = 'thumbnail_img'
         AND pi.width = 600 AND pi.format_id = 90
+),
+
+last_sale AS (
+    SELECT
+        tp.product,
+        min(oi.order_date) AS first_sale,
+        max(oi.order_date) AS last_sale
+    FROM
+        public.tenant_products AS tp
+    INNER JOIN
+        public.order_items AS oi
+        ON tp.tenant_product_id = oi.tenant_product_id
+    GROUP BY
+        tp.product
 )
 
 SELECT
@@ -22,7 +36,11 @@ SELECT
     im.match_angle,
     ti1.name AS search_image_type,
     ti2.name AS match_image_type,
-    im.score AS image_score
+    im.score AS image_score,
+    ls_search.first_sale AS search_first_sale,
+    ls_search.last_sale AS search_last_sale,
+    ls_match.first_sale AS match_first_sale,
+    ls_match.last_sale AS match_last_sale
 FROM
     datascience._2_get_info_of_entity_products_with_img_matches(
         'tenant',
@@ -36,4 +54,8 @@ FROM
 INNER JOIN thumbnail_imgs AS ti1
     ON im.image_id = ti1.image_id
 INNER JOIN thumbnail_imgs AS ti2
-    ON im.match_image_id = ti2.image_id;
+    ON im.match_image_id = ti2.image_id
+INNER JOIN last_sale AS ls_search
+    ON im.product = ls_search.product
+INNER JOIN last_sale AS ls_match
+    ON im.match_product = ls_match.product;
